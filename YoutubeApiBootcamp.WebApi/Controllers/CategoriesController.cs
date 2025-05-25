f20a6ca8ee2641b6542a1411674d3f0d67fc0a2e
Your Name <you@example.com>
2025-05-25 22:14:23 +0300

    Add project files.

---
+++ YoutubeApiBootcamp.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YoutubeApiBootcamp.WebApi.Context;
+using YoutubeApiBootcamp.WebApi.Entities;
+
+namespace YoutubeApiBootcamp.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CategoriesController : ControllerBase
+	{
+		private readonly ApiContext _context;
+
+		public CategoriesController(ApiContext context)
+		{
+			_context = context;
+		}
+
+		[HttpPost]
+		public IActionResult CreateCategory(Category category)
+		{
+			_context.Categories.Add(category);
+			_context.SaveChanges();
+
+			return Ok("Category Added Succesfully");
+		}
+
+		[HttpGet]
+		public IActionResult GetCategories()
+		{
+			var values = _context.Categories.ToList();
+
+			return Ok(values);
+		}
+
+		[HttpDelete]
+		public IActionResult DeleteCategory(int categoryId)
+		{
+			var deletedCategory = _context.Categories.Find(categoryId);
+			_context.Categories.Remove(deletedCategory);
+			_context.SaveChanges();
+
+			return Ok("Category Deleted Succesfully");
+		}
+
+		[HttpGet("GetById")]
+		public IActionResult GetCategoryById(int id)
+		{
+			var category = _context.Categories.Find(id);
+
+			return Ok(category);
+		}
+
+		[HttpPut]
+		public IActionResult UpdateCategory(Category category)
+		{
+			_context.Categories.Update(category);
+			_context.SaveChanges();
+
+			return Ok("Category Updated Succesfully");
+		}
+	}
+}
---
+++ YoutubeApiBootcamp.WebApi/Controllers/ChefsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YoutubeApiBootcamp.WebApi.Context;
+using YoutubeApiBootcamp.WebApi.Entities;
+
+namespace YoutubeApiBootcamp.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ChefsController : ControllerBase
+	{
+		private readonly ApiContext _context;
+
+		public ChefsController(ApiContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public IActionResult GetChefList()
+		{
+			var values = _context.Chefs.ToList();
+
+			return Ok(values);
+		}
+
+		[HttpPost]
+		public IActionResult CreateChef(Chef chef)
+		{
+			_context.Chefs.Add(chef);
+			_context.SaveChanges();
+
+			return Ok("Chef Added Succesfully");
+		}
+
+		[HttpDelete]
+		public IActionResult DeleteChef(int chefId)
+		{
+			var deletedChef = _context.Chefs.Find(chefId);
+			_context.Chefs.Remove(deletedChef);
+			_context.SaveChanges();
+
+			return Ok("Chef Deleted Succesfully");
+		}
+
+		[HttpGet("GetById")]
+		public IActionResult GetChefById(int id)
+		{
+			return Ok(_context.Chefs.Find(id));
+		}
+
+		[HttpPut]
+		public IActionResult UpdateChef(Chef chef)
+		{
+			_context.Chefs.Update(chef);
+			_context.SaveChanges();
+
+			return Ok("Chef Updated Succesfully");
+		}
+	}
+}
