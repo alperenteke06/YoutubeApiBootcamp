8101add7d467e31f1e6285ef7c04d9d6e82e1991
alperenteke06 <alperen.teke06@gmail.com>
2025-05-26 17:11:01 +0300

    FluentValidation kurulumu ve ProductValidator sınıfının oluşturulması.

---
+++ YoutubeApiBootcamp.WebApi/ValidationRules/ProductValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using YoutubeApiBootcamp.WebApi.Entities;
+
+namespace YoutubeApiBootcamp.WebApi.ValidationRules
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.ProductName).NotEmpty().WithMessage("Ürün adı boş bırakılamaz!");
+            RuleFor(x => x.ProductName).MinimumLength(2).WithMessage("En az 2 karakter veri girişi yapınız!");
+            RuleFor(x => x.ProductName).MaximumLength(50).WithMessage("En fazla 50 karakter veri girişi yapınız!");
+
+            RuleFor(x => x.ProductPrice).NotEmpty().WithMessage("Ürün fiyatı boş geçilemez!").LessThan(0).WithMessage("Ürün fiyatı negatif olamaz!")
+                .GreaterThan(1000).WithMessage("Ürün fiyatı bu kadar yüksek olamaz, lütfen girdiğiniz değeri kontrol ediniz!");
+
+            RuleFor(x => x.ProductDescription).NotEmpty().WithMessage("Ürün açıklaması boş bırakılamaz!");
+        }
+    }
+}
