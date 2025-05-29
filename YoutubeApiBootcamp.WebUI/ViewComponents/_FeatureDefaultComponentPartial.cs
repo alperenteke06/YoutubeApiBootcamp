5621f09415b70ecd4a5f8d8f25a9110105ea68f3
Your Name <you@example.com>
2025-05-30 01:17:34 +0300

    UI Projesinin ve View Componentlerin oluşturulması.

---
+++ YoutubeApiBootcamp.WebUI/ViewComponents/_FeatureDefaultComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace YoutubeApiBootcamp.WebUI.ViewComponents
+{
+	public class _FeatureDefaultComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
---
+++ YoutubeApiBootcamp.WebUI/ViewComponents/_HeadDefaultComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace YoutubeApiBootcamp.WebUI.ViewComponents
+{
+	public class _HeadDefaultComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
