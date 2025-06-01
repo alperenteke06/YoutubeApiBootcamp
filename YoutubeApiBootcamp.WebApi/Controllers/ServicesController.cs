02fb1af427c83bd8a311eb2931576999886dd509
Your Name <you@example.com>
2025-06-01 20:47:02 +0300

    Servislerin consume edilmesi.

---
+++ YoutubeApiBootcamp.WebApi/Controllers/ServicesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YoutubeApiBootcamp.WebApi.Context;
+using YoutubeApiBootcamp.WebApi.Entities;
+
+namespace YoutubeApiBootcamp.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ServicesController : Controller
+	{
+		private readonly ApiContext _context;
+
+		public ServicesController(ApiContext context)
+		{
+			_context = context;
+		}
+
+		[HttpPost]
+		public IActionResult CreateService(Service service)
+		{
+			_context.Services.Add(service);
+			_context.SaveChanges();
+
+			return Ok("Service Added Succesfully");
+		}
+
+		[HttpGet]
+		public IActionResult GetServices()
+		{
+			var values = _context.Services.ToList();
+
+			return Ok(values);
+		}
+
+		[HttpDelete]
+		public IActionResult DeleteService(int serviceId)
+		{
+			var deletedService = _context.Services.Find(serviceId);
+			_context.Services.Remove(deletedService);
+			_context.SaveChanges();
+
+			return Ok("Service Deleted Succesfully");
+		}
+
+		[HttpGet("GetById")]
+		public IActionResult GetServiceById(int id)
+		{
+			var service = _context.Services.Find(id);
+
+			return Ok(service);
+		}
+
+		[HttpPut]
+		public IActionResult UpdateService(Service service)
+		{
+			_context.Services.Update(service);
+			_context.SaveChanges();
+
+			return Ok("Service Updated Succesfully");
+		}
+	}
+}
--- YoutubeApiBootcamp.WebUI/ViewComponents/_ServiceDefaultComponentPartial.cs
+++ YoutubeApiBootcamp.WebUI/ViewComponents/_ServiceDefaultComponentPartial.cs
@@ -1,11 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using YoutubeApiBootcamp.WebUI.Dtos.ServiceDtos;
 
 namespace YoutubeApiBootcamp.WebUI.ViewComponents
 {
 	public class _ServiceDefaultComponentPartial:ViewComponent
 	{
-		public IViewComponentResult Invoke()
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _ServiceDefaultComponentPartial(IHttpClientFactory httpClientFactory)
 		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+
+			var resMessage = await client.GetAsync("https://localhost:7168/api/Services");
+
+			if (resMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await resMessage.Content.ReadAsStringAsync();
+
+				var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+
+				return View(values);
+			}
+
 			return View();
 		}
 	}
