4b5f559d41a78fcfe35ea3c8f07cd88afa918350
Your Name <you@example.com>
2025-06-10 23:49:10 +0300

    Menünün Consume Edilmesi.

---
+++ YoutubeApiBootcamp.WebUI/ViewComponents/DefaultMenuViewComponents/_DefaultMenuProductComponentPartial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using YoutubeApiBootcamp.WebUI.Dtos.ProductDtos;
+
+namespace YoutubeApiBootcamp.WebUI.ViewComponents.DefaultMenuViewComponents
+{
+	public class _DefaultMenuProductComponentPartial:ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _DefaultMenuProductComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+
+			var resMessage = await client.GetAsync("https://localhost:7168/api/Products/");
+
+			if (resMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await resMessage.Content.ReadAsStringAsync();
+
+				var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+
+				return View(values);
+			}
+
+			return View();
+		}
+	}
+}
