05874d96bc081a3e77b2f1cbb042884c3e280bc0
Your Name <you@example.com>
2025-06-12 00:02:30 +0300

    Testimonial Api ve Consume İşlemleri.

---
+++ YoutubeApiBootcamp.WebApi/Controllers/TestimonialsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YoutubeApiBootcamp.WebApi.Context;
+using YoutubeApiBootcamp.WebApi.Entities;
+
+namespace YoutubeApiBootcamp.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TestimonialsController : ControllerBase
+	{
+		private readonly ApiContext _context;
+
+		public TestimonialsController(ApiContext context)
+		{
+			_context = context;
+		}
+
+		[HttpPost]
+		public IActionResult CreateTestimonial(Testimonial testimonial)
+		{
+			_context.Testimonials.Add(testimonial);
+			_context.SaveChanges();
+
+			return Ok("Testimonial Added Succesfully");
+		}
+
+		[HttpGet]
+		public IActionResult GetTestimonials()
+		{
+			var values = _context.Testimonials.ToList();
+
+			return Ok(values);
+		}
+
+		[HttpDelete]
+		public IActionResult DeleteTestimonial(int testimonialId)
+		{
+			var deletedTestimonial = _context.Testimonials.Find(testimonialId);
+			_context.Testimonials.Remove(deletedTestimonial);
+			_context.SaveChanges();
+
+			return Ok("Testimonial Deleted Succesfully");
+		}
+
+		[HttpGet("GetById")]
+		public IActionResult GetTestimonialById(int id)
+		{
+			var Testimonial = _context.Testimonials.Find(id);
+
+			return Ok(Testimonial);
+		}
+
+		[HttpPut]
+		public IActionResult UpdateTestimonial(Testimonial testimonial)
+		{
+			_context.Testimonials.Update(testimonial);
+			_context.SaveChanges();
+
+			return Ok("Testimonial Updated Succesfully");
+		}
+	}
+}
