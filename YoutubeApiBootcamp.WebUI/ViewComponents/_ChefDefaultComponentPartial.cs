c7579c244d708bc47183337ff709005ac2378544
Your Name <you@example.com>
2025-06-15 06:30:13 +0300

    Chef API hazırlanması ve consume edilmesi. Footer alanının ViewComponent olarak statik veriler ile eklenmesi.

---
+++ YoutubeApiBootcamp.WebUI/ViewComponents/_ChefDefaultComponentPartial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using YoutubeApiBootcamp.WebUI.Dtos;
+
+namespace YoutubeApiBootcamp.WebUI.ViewComponents
+{
+	public class _ChefDefaultComponentPartial:ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _ChefDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+
+			var resMessage = await client.GetAsync("https://localhost:7168/api/Chefs/");
+
+			if (resMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await resMessage.Content.ReadAsStringAsync();
+
+				var values = JsonConvert.DeserializeObject<List<ResultChefDto>>(jsonData);
+
+				return View(values);
+			}
+
+			return View();
+		}
+	}
+}
